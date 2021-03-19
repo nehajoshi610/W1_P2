ceba4118dfbaf81aee38a810b73c7948eeb58c98
Admin <Admin@DESKTOP-5RIGV7J>
2021-03-20 01:03:38 +0530

    time of simulation 100 sec is set.and solver is auto ode45

--- truck.m
+++ truck.m
@@ -3,7 +3,5 @@ m2=320;
 K1=50000;
 K2=50000;
 b1=350;
-
-t[0:0.01:10];
-t[0:0.01:10]';
-stin=(zeros(floor((length(t)/2)+1),1):5*ones(floor(length(t)/2),1));
+t=(0:0.01:10)';
+stin=(zeros(floor((length(t)/2)+1),1):10*ones(floor(length(t)/2),1));
